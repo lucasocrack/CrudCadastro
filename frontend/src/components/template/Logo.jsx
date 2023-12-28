2f67b90188faff3c212abe06c381884306bcbdf6
Lucas Campos <bhlucascampos@gmail.com>
2023-12-28 00:52:11 -0300

    end of creation base structure

--- frontend/src/components/template/Logo.jsx
+++ frontend/src/components/template/Logo.jsx
@@ -1,7 +1,11 @@
 import './Logo.css'
+import logo from '../../assets/img/logo.png'
 import React from 'react'
+
 
 export default props =>
     <aside className="logo">
-        Logo
+        <a href="/" className="logo">
+            <img src={logo} alt="logo" />
+        </a>
     </aside>
